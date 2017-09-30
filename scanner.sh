6c1559157e002a152421b16902735c4fe5b50e57
Sarunas Ramanauskas <Sarunas.Ramanauskas@ibm.com>
2017-09-30 20:34:34 +0300

    grammar & scanner primitive files created

---
+++ scanner.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+
+# Scanner
+# the compiler pass that converts a string of chatacters into stream of words
+
+
+
+echo "When I get older, I will scan programs sources codes and will divide text into tokens(words)"
